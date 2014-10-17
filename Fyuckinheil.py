dee70619b0099b41d69eba68286bcde011e9f15a
EliasRoberts <eliasroberts99@gmail.com>
2014-10-17 13:41:04 -0500

    ChinuckLand

---
+++ Fyuckinheil.py
@@ -0,0 +1,4 @@
+print "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHH! furkienheil froom calciuoses!"
+celcius = int(raw_input())
+farenhiet = ((celcius * 9) / 5 + 32)
+print "celescius to fyuckinhiel is " + str(farenhiet)
